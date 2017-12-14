d3b267b2267cbfa29551ea1df66035b3e6c1c66c
Darsky <276294393@qq.com>
2017-12-14 11:02:27 +0800

    readme

---
+++ iOSDesignPattern/iOSDesignPattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSDesignPattern
+//
+//  Created by Darsky on 2017/12/14.
+//  Copyright © 2017年 Darsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSDesignPattern/iOSDesignPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSDesignPattern
+//
+//  Created by Darsky on 2017/12/14.
+//  Copyright © 2017年 Darsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
